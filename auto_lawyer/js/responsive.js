4200e236225c8617b8a2cc2fd65e768ac3ec5b9c
kubar <lolilop919@aport.ru>
2018-05-28 10:57:17 +0500

    first commit

---
+++ auto_lawyer/js/responsive.js
@@ -0,0 +1,17 @@
+@media screen and (max-width: 1024px){
+    article{
+        width: 90%;
+    }
+    
+    .icon1, .icon2{
+        display: none; 
+    } 
+}
+ 
+ @media screen and (max-width: 768px){
+    
+}
+
+@media screen and (max-width: 1024px){
+    
+} 
